91f471cdd02741c0683c3ca7033946c648416c20
lazaressathya <lazaressathya143@gmail.com>
2022-04-30 11:54:11 +0530

    again modified in dev env

--- dev/main.tf
+++ dev/main.tf
@@ -1,19 +1,19 @@
-module "vpc" {
+module "my_vpc" {
     source = "../modules/vpc"
     vpc_cidr = "192.168.0.0/16"
     tenancy = "default"
-    vpc_id="${module.vpc.vpc_id}"
+    vpc_id="${module.my_vpc.vpc_id}"
     subnet_cidr="192.168.1.0/24"
 
 
   
 }
 
-module "ec2" {
+module "my_ec2" {
     source = "../modules/ec2"
     ec2_count= 1
     instance_type="t2.micro"
-    subnet_id ="${module.vpc.subnet_id}"
+    subnet_id ="${module.my_vpc.subnet_id}"
     ami_id ="ami-5a8da73d"
 
 
